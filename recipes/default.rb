b9485cd0797185dee3dadc51de1fccb846b53af0
Nico Di Rocco <n.dirocco@tech.leaseweb.com>
2015-05-31 15:16:35 +0200

    Accidentally removed too much

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,26 @@
 #
 # Cookbook Name:: supervisord
 # Recipe:: default
+#
+# Copyright 2013, Chris Soyars
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "supervisor" do
+  action :install
+
+  notifies :stop, "service[supervisor]", :immediately
+end
+
+service "supervisor" do
+  supports :status => true, :restart => false, :reload => true
+  reload_command "supervisorctl update"
+  action :enable
+end
+
+unix_http_server = node['supervisord']['unix_http_server'].select { |k, v| k unless v.nil? }
+unix_http_server = (unix_http_server unless unix_http_server.map { |_, v| not v.nil? }.select { |a| a }.empty?)
 
 inet_http_server = node['supervisord']['inet_http_server'].select { |k, v| k unless v.nil? }
 inet_http_server = (inet_http_server unless inet_http_server.map { |_, v| !v.nil? }.select { |a| a }.empty?)
