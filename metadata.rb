a7a37e26e62c01f8b3d12dd386afdbc23dcbd5d3
Nico Di Rocco <n.dirocco@tech.leaseweb.com>
2015-07-16 13:50:10 +0200

    Bumped version to 0.2.2 (really this time)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ctso@ctso.me'
 license          'All rights reserved'
 description      'Installs/Configures supervisord'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 supports         'ubuntu', '= 12.04'
 supports         'ubuntu', '= 14.04'
